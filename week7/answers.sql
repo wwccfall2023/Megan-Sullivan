e79d9f7100901cb06af1539ca1c8020eaadde54c
Megan <94812744+Megan-Sullivan@users.noreply.github.com>
2023-11-28 18:14:41 -0800

    More syntax fixes

--- week7/answers.sql
+++ week7/answers.sql
@@ -14,8 +14,8 @@ CREATE TABLE characters (
   character_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   player_id INT UNSIGNED NOT NULL,
   `name` VARCHAR(30) NOT NULL,
-  `level` INT UNSIGNED NOT NULL
-  CONSTRAINT  player_id_fk_players
+  `level` INT UNSIGNED NOT NULL,
+  CONSTRAINT  characters_fk_players
         FOREIGN KEY (player_id)
         REFERENCES players (player_id)
         ON UPDATE CASCADE
