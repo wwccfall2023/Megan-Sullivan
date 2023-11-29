489ae11010aa1d4a72115d4d6ca77e1a8788e252
Megan <94812744+Megan-Sullivan@users.noreply.github.com>
2023-11-29 15:10:01 -0800

    team members fix and keys unrenamed

--- week7/answers.sql
+++ week7/answers.sql
@@ -13,8 +13,8 @@ CREATE TABLE players (
 CREATE TABLE characters (
   character_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   player_id INT UNSIGNED NOT NULL,
-  character_name VARCHAR(30) NOT NULL,
-  character_level INT UNSIGNED NOT NULL,
+  `name` VARCHAR(30) NOT NULL,
+  `level` INT UNSIGNED NOT NULL,
   CONSTRAINT  characters_fk_players
     FOREIGN KEY (player_id)
     REFERENCES players (player_id)
@@ -24,7 +24,7 @@ CREATE TABLE characters (
 
 CREATE TABLE winners ( 
   character_id INT UNSIGNED,
-  winner_name VARCHAR(30) NOT NULL,
+  `name` VARCHAR(30) NOT NULL,
   CONSTRAINT winners_fk_characters
     FOREIGN KEY (character_id) 
     REFERENCES characters (character_id)
@@ -34,8 +34,8 @@ CREATE TABLE winners (
 
 CREATE TABLE character_stats (
   character_id INT UNSIGNED,
-  character_health INT UNSIGNED NOT NULL,
-  character_armor INT UNSIGNED NOT NULL,
+  health INT UNSIGNED NOT NULL,
+  armor INT UNSIGNED NOT NULL,
   CONSTRAINT character_stats_fk_characters
     FOREIGN KEY (character_id) 
     REFERENCES characters (character_id)
@@ -45,15 +45,21 @@ CREATE TABLE character_stats (
 
 CREATE TABLE teams (
   team_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  team_name VARCHAR(30) NOT NULL
+  `name` VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE team_members (
   team_member_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   team_id INT UNSIGNED,
+  character_id INT UNSIGNED,
   CONSTRAINT team_members_fk_teams
     FOREIGN KEY (team_id)
     REFERENCES teams (team_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+  CONSTRAINT team_members_fk_characters
+    FOREIGN KEY (character_id) 
+    REFERENCES characters (character_id)
     ON UPDATE CASCADE
     ON DELETE CASCADE
 );
