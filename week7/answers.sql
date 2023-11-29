450bbfea679caa9810a9c83b8c64019e19c0c673
Megan <94812744+Megan-Sullivan@users.noreply.github.com>
2023-11-28 17:45:42 -0800

    Created players table.

--- week7/answers.sql
+++ week7/answers.sql
@@ -1,3 +1,11 @@
 -- Create your tables, views, functions and procedures here!
+DROP SCHEMA IF EXISTS destruction;
 CREATE SCHEMA destruction;
 USE destruction;
+
+CREATE TABLE players (
+  player_id INT UNSIGNED PRIMARY KEY NO NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL
+);
