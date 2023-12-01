8f2d2be9a6b94b4cdfb13c6be96cea42f1f94c6a
Megan <94812744+Megan-Sullivan@users.noreply.github.com>
2023-11-30 18:16:54 -0800

    added attack procedure
    probably not going to work properly, there's some minor syntax issues and I still have to go back through my views and fix several things

--- week7/answers.sql
+++ week7/answers.sql
@@ -131,7 +131,7 @@ GROUP BY t.team_id, i.item_id -- Deduplicate the items by team and item
 ORDER BY t.team_id, i.name;
 
 -- Create a function named armor_total
-DELIMITER //
+DELIMITER ;;
 CREATE FUNCTION armor_total(character_id INT UNSIGNED)
 RETURNS INT
 DETERMINISTIC
@@ -149,11 +149,49 @@ BEGIN
   WHERE e.character_id = character_id;
   -- Return the total armor
   RETURN total_armor;
-END //
-DELIMITER /
+END;;
+DELIMITER ;
 
-
-
-
-
+DELIMITER ;;
+-- Create a procedure named attack
+CREATE PROCEDURE attack(id_of_character_being_attacked INT UNSIGNED, id_of_equipped_item_used_for_attack INT UNSIGNED)
+BEGIN
+  -- Declare variables to store the armor, damage, and health of the character being attacked
+  DECLARE armor INT DEFAULT 0;
+  DECLARE damage INT DEFAULT 0;
+  DECLARE health INT DEFAULT 0;
+  -- Call the armor_total function to get the armor of the character being attacked
+  SET armor = armor_total(id_of_character_being_attacked);
+  -- Get the damage of the item being used to attack from the items table
+  SELECT damage INTO damage
+  FROM items
+  WHERE item_id = id_of_equipped_item_used_for_attack;
+  -- Subtract the armor from the damage to get the net damage
+  SET damage = damage - armor;
+  -- If the net damage is positive, proceed to update the character's health
+  IF damage > 0 THEN
+    -- Get the current health of the character being attacked from the character_stats table
+    SELECT health INTO health
+    FROM character_stats
+    WHERE character_id = id_of_character_being_attacked;
+    -- Subtract the net damage from the current health to get the new health
+    SET health = health - damage;
+    -- If the new health is positive, update the character_stats table with the new health
+    IF health > 0 THEN
+      UPDATE character_stats
+      SET health = health
+      WHERE character_id = id_of_character_being_attacked;
+    -- Else, if the new health is zero or negative, delete the character from the database
+    ELSE
+      -- Delete the character from the characters table
+      -- This will also delete the character from the winners, character_stats, team_members, inventory, and equipped tables due to the cascade option on the foreign keys
+      DELETE FROM characters
+      WHERE character_id = id_of_character_being_attacked;
+    END IF;
+  -- Else, if the net damage is zero or negative, do nothing
+  ELSE
+    -- No action needed
+  END IF;
+END;
+DELIMITER ;
 
