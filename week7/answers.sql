277ed2692ab1fbe79361b0088c7f4d7fb4c0917a
Megan <94812744+Megan-Sullivan@users.noreply.github.com>
2023-12-03 00:52:20 -0800

    armor_total function rollback

--- week7/answers.sql
+++ week7/answers.sql
@@ -143,10 +143,10 @@ BEGIN
   FROM character_stats cs
   WHERE character_id = cs.character_id;
   -- Add the armor from the items the character has equipped
-  SELECT SUM(e.armor) INTO total_armor
+  SELECT SUM(i.armor) INTO total_armor
   FROM equipped e
   INNER JOIN items i ON e.item_id = i.item_id
-  WHERE i.character_id = e.character_id;
+  WHERE e.character_id = character_id;
   -- Return the total armor
   RETURN total_armor;
 END;;
