6752d985b32805f676f4e2e9a8ed0ef1e0854d3a
Megan <94812744+Megan-Sullivan@users.noreply.github.com>
2023-12-13 00:26:43 -0800

    Added social tables

--- week9/answers.sql
+++ week9/answers.sql
@@ -1,3 +1,69 @@
 -- Create your tables, views, functions and procedures here!
+DROP SCHEMA IF EXISTS social;
 CREATE SCHEMA social;
 USE social;
+
+CREATE TABLE users (
+  user_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE sessions (
+	session_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED NOT NULL,
+    created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT  sessions_fk_users
+    FOREIGN KEY (user_id)
+    REFERENCES users (user_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+CREATE TABLE friends (
+	user_friend_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED NOT NULL,
+	friend_id INT UNSIGNED NOT NULL,
+	CONSTRAINT  friends_user_fk_users
+    FOREIGN KEY (user_id)
+    REFERENCES users (user_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+    CONSTRAINT  friends_friend_fk_users
+    FOREIGN KEY (friend_id)
+    REFERENCES users (user_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+CREATE TABLE posts (
+	post_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED NOT NULL,
+    created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    content VARCHAR(255) NOT NULL,
+    CONSTRAINT  posts_fk_users
+    FOREIGN KEY (user_id)
+    REFERENCES users (user_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+CREATE TABLE notifications (
+	notification_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED NOT NULL,
+    post_id INT UNSIGNED NOT NULL,
+    CONSTRAINT  notifications_fk_users
+    FOREIGN KEY (user_id)
+    REFERENCES users (user_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+    CONSTRAINT  notification_fk_posts
+    FOREIGN KEY (post_id)
+    REFERENCES posts (post_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+)
