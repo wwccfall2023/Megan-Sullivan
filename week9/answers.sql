7c8cf63c316e0f34ccc106805fa039209140612e
Megan <94812744+Megan-Sullivan@users.noreply.github.com>
2023-12-14 16:45:28 -0800

    Testing notification_posts VIEW

--- week9/answers.sql
+++ week9/answers.sql
@@ -78,6 +78,22 @@ CREATE TABLE notifications (
     ON DELETE CASCADE
 );
 
+
+
+-- Create notification_posts VIEW
+CREATE VIEW notification_posts AS
+SELECT 
+	n.user_id,
+	u.first_name,
+	u.last_name,
+	p.post_id,
+	p.content
+FROM notifications n
+INNER JOIN users u ON n.user_id = u.user_id
+INNER JOIN posts p ON n.post_id = p.post_id;
+
+
+
 DELIMITER ;;
 CREATE TRIGGER after_user_insert
 AFTER INSERT ON users
@@ -91,10 +107,10 @@ BEGIN
     DECLARE users_cursor CURSOR FOR 
 		SELECT user_id 
 		FROM users 
-			WHERE user_id != NEW.user_id;
+		WHERE user_id != NEW.user_id;
             
 	DECLARE CONTINUE HANDLER FOR NOT FOUND
-		SET row_not_found = TRUE;
+	SET row_not_found = TRUE;
         
 	INSERT INTO posts (user_id, content) VALUES (NEW.user_id, 'just joined!');
 	SET new_post_id = LAST_INSERT_ID();
@@ -102,80 +118,18 @@ BEGIN
 	DELETE FROM notifications;
     
     OPEN users_cursor;
-	user_loop: LOOP
+    user_loop: LOOP
     FETCH users_cursor INTO new_user_id;
     IF row_not_found THEN
-			LEAVE user_loop;
+	LEAVE user_loop;
 		END IF;
     INSERT INTO notifications (user_id, post_id) VALUES (new_user_id, new_post_id);
   END LOOP;
-  CLOSE users_cursor;
+  CLOSE users_cursor; -- LOOK I CLOSED MY CURSOR THIS TIME!!! :D
 END;;
 DELIMITER ;
 
 
-
-/*
--- Create 'users' table by Bing AI
-CREATE TABLE users (
-  user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- Create 'sessions' table
-CREATE TABLE sessions (
-  session_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  user_id INT NOT NULL,
-  created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  CONSTRAINT sessions_fk_users FOREIGN KEY (user_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE
-);
-
--- Create 'friends' table
-CREATE TABLE friends (
-  user_friend_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  user_id INT NOT NULL,
-  friend_id INT NOT NULL,
-  CONSTRAINT friends_fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
-  CONSTRAINT friends_fk_friend_id FOREIGN KEY (friend_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE
-);
-
--- Create 'posts' table
-CREATE TABLE posts (
-  post_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  user_id INT NOT NULL,
-  created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  content TEXT NOT NULL,
-  CONSTRAINT posts_fk_users FOREIGN KEY (user_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE
-);
-
--- Create 'notifications' table
-CREATE TABLE notifications (
-  notification_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  user_id INT NOT NULL,
-  post_id INT NOT NULL,
-  CONSTRAINT notifications_fk_users FOREIGN KEY (user_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
-  CONSTRAINT notifications_fk_posts FOREIGN KEY (post_id) REFERENCES posts(post_id) ON UPDATE CASCADE ON DELETE CASCADE
-);
-*/
-
-/*
--- Create notification_posts VIEW
-CREATE VIEW notification_posts AS
-SELECT 
-	n.user_id,
-	u.first_name,
-	u.last_name,
-	p.post_id,
-	p.content
-FROM notifications n
-INNER JOIN users u ON n.user_id = u.user_id
-INNER JOIN posts p ON n.post_id = p.post_id;
-*/
 
 /*
 DELIMITER ;;
